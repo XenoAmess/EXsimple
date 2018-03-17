cc788260b016cda4738024f3c5ca9ff27c4238f3
XenoAmess <912890489@qq.com>
2018-03-17 19:23:35 +0800

    fix

--- tools/initnuitka.sh
+++ tools/initnuitka.sh
@@ -2,5 +2,5 @@
 cd /
 wget -O /tools/python/exsimple.py https://raw.githubusercontent.com/XenoAmess/EXsimple/master/src/exsimple.py
 sed -i "s/THIS_IS_DAILYPASTE = False;/THIS_IS_DAILYPASTE = True;/g" /tools/python/exsimple.py
-nuitka --standalone  --recurse-all  --python-version=3.5  /tools/python/exsimple.py
-sudo screen -dmS exsimple sudo /exsimple.dist/exsimple.exe dir=/DailyPaste
+nuitka --standalone  --recurse-all  --python-version=3.5  /tools/python/exsimple.py  --output-dir=/tools/  --remove-output
+sudo screen -dmS exsimple sudo /tools/exsimple.dist/exsimple.exe dir=/DailyPaste
