80e1c51a539c29f34e112b21216d82e4614e0a2e
XenoAmess <912890489@qq.com>
2018-03-20 19:33:36 +0800

    update initnuitka.sh

--- tools/initnuitka.sh
+++ tools/initnuitka.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+screen -S exsimple -X quit
 cd /
 wget -O /tools/python/exsimple.py https://raw.githubusercontent.com/XenoAmess/EXsimple/master/src/exsimple.py
 sed -i "s/THIS_IS_DAILYPASTE = False;/THIS_IS_DAILYPASTE = True;/g" /tools/python/exsimple.py
